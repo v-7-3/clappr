f1e5aebbe9739920a4a1b316cc75cb02a2d6d93a
v-7-3 <57416994+v-7-3@users.noreply.github.com>
2024-02-23 11:46:53 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-test
+<script>
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'your-url-here.txt', true);
+
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        // get the text from response.
+        const textContent = xhr.responseText;
+        console.log(textContent);
+    }
+};
+
+xhr.send();
+</script>
