9c99167f16762bc64cf609c5af291630bdf88ba7
v-7-3 <57416994+v-7-3@users.noreply.github.com>
2024-02-23 12:52:25 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
-<script>
     setTimeout(function() {
         player.configure({source: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8"});
         player.play();
     }, 10000);
 
-</script>
